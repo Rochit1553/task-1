0e93f8308fcd5c31c3697660f2833ffa1a07424c
Rochit1553 <rochitshegokar1553@gmail.com>
2025-07-20 15:03:45 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("addition:",add,"\nsubstraction:",sub,"\nmultiplication:",mul,"\ndivision:",div)
---
+++ task2.py
@@ -0,0 +1,4 @@
+a=(input("enter your first name"))
+b=(input("enter your last name"))
+fullname=a+" "+b
+print(f"hello, {fullname}! welcome to python program.")
